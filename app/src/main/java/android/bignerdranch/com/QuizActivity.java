fb367be9a68336c22ee4ca2e80b036f40e4e9ec5
sam shah <s_s406@student.edu.au>
2019-08-04 14:55:37 +1000

    GeoQuiz Week 1

---
+++ app/src/main/java/android/bignerdranch/com/QuizActivity.java
@@ -0,0 +1,39 @@
+package android.bignerdranch.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id. true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id. false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/android/bignerdranch/com/Toast.java
@@ -0,0 +1,7 @@
+package android.bignerdranch.com;
+
+import android.content.Context;
+
+public class Toast {
+    public static Toast makeText(Context context,int resID,int duration)
+}
